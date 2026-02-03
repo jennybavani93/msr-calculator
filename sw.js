81dcc0c464bd38f21031c72cd83b76391328448a
jennybavani93 <jennybavani@gmail.com>
2026-02-03 16:20:55 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "msr-pos-v3";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./sw.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
